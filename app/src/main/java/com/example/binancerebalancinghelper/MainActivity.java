ca25839273bb33ba6f4108e65e4fd98b32dd64ad
vipnet1 <timur.pichkhadze@gmail.com>
2023-08-05 18:35:25 +0300

    add ignore battery optimizations

--- app/src/main/java/com/example/binancerebalancinghelper/MainActivity.java
+++ app/src/main/java/com/example/binancerebalancinghelper/MainActivity.java
@@ -8,5 +8,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BatteryHelper batteryHelper = new BatteryHelper(this);
+        batteryHelper.requestIgnoreBatteryOptimizationsIfNeeded();
     }
 }
