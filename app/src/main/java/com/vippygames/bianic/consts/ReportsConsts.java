eb00b3152ffe6ef6b07a95a46285e8770dfd9647
vipnet1 <timur.pichkhadze@gmail.com>
2023-09-09 23:20:51 +0300

    reduce max records exceptions log and reports

--- app/src/main/java/com/vippygames/bianic/consts/ReportsConsts.java
+++ app/src/main/java/com/vippygames/bianic/consts/ReportsConsts.java
@@ -2,6 +2,6 @@ package com.vippygames.bianic.consts;
 
 public class ReportsConsts {
     public static final String REPORT_RECORD_UUID_INTENT_EXTRA = "reportRecordUuid";
-    public static final int MAX_STORED_REPORTS_COUNT = 100;
-    public static final int STORED_REPORTS_DELETE_PER_QUERY = 25;
+    public static final int MAX_STORED_REPORTS_COUNT = 30;
+    public static final int STORED_REPORTS_DELETE_PER_QUERY = 10;
 }
