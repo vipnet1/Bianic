8c1506ad3bd4238f3d85611a198e6885ed39bb1b
vipnet1 <timur.pichkhadze@gmail.com>
2023-09-09 12:34:44 +0300

    repor record xml, making uid views from invisible to gone

--- app/src/main/java/com/vippygames/bianic/rebalancing/validation/exceptions/UnvalidatedRecordsException.java
+++ app/src/main/java/com/vippygames/bianic/rebalancing/validation/exceptions/UnvalidatedRecordsException.java
@@ -3,7 +3,7 @@ package com.vippygames.bianic.rebalancing.validation.exceptions;
 public class UnvalidatedRecordsException extends Exception {
     @Override
     public String getMessage() {
-        return "You need to validate records by clicking the button in main page.";
+        return "You need to validate records by clicking the button in the main page.";
     }
 }
 
