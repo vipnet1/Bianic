5464d605965ecd2b6a378a7904c79cfafd3a6da0
vipnet1 <timur.pichkhadze@gmail.com>
2023-09-15 10:28:31 +0300

    change NotificationType to include channels data

--- app/src/main/java/com/vippygames/bianic/notifications/NotificationType.java
+++ app/src/main/java/com/vippygames/bianic/notifications/NotificationType.java
@@ -1,25 +1,42 @@
 package com.vippygames.bianic.notifications;
 
 public enum NotificationType {
-    REBALANCING_RUNNING(1, 1),
-    REBALANCING_AVAILABLE(2, 2),
-    NORMAL_EXCEPTION(3, 4),
-    CRITICAL_EXCEPTION(5, 6),
-    FATAL_EXCEPTION(7, 8);
+    REBALANCING_RUNNING("rebalance_check_running_channel", "Rebalance Check Running"
+            , "Indicates that performing rebalance check in background. Required for it to work.", 1),
+    REBALANCING_AVAILABLE("rebalance_available_channel", "Rebalance Available",
+            "Reached threshold you set you want to rebalance at.", 1),
+    NORMAL_EXCEPTION("normal_exception_channel", "Normal Exception",
+            "Fixable problem occurred.", 1),
+    CRITICAL_EXCEPTION("critical_exception_channel", "Critical Exception",
+            "Problem we didn't expect to happen.", 1),
+    FATAL_EXCEPTION("fatal_exception_channel", "Fatal Exception",
+            "Problem storing other exceptions.", 1);
 
-    private final int minNotificationId;
-    private final int maxNotificationId;
+    private final String channelId;
+    private final String channelName;
+    private final String channelDescription;
+    private final int maxNotificationsCount;
 
-    public int getMinNotificationId() {
-        return minNotificationId;
+    public String getChannelId() {
+        return channelId;
     }
 
-    public int getMaxNotificationId() {
-        return maxNotificationId;
+    public String getChannelName() {
+        return channelName;
     }
 
-    NotificationType(int minNotificationId, int maxNotificationId) {
-        this.minNotificationId = minNotificationId;
-        this.maxNotificationId = maxNotificationId;
+    public String getChannelDescription() {
+        return channelDescription;
+    }
+
+    public int maxNotificationsCount() {
+        return maxNotificationsCount;
+    }
+
+    NotificationType(String channelId, String channelName, String channelDescription, int maxNotificationsCount) {
+        this.channelId = channelId;
+        this.channelName = channelName;
+        this.channelDescription = channelDescription;
+        this.maxNotificationsCount = maxNotificationsCount;
     }
 }
