44ca4ec4030beb5995f6cce7e5cfbb09f881ee90
vipnet1 <timur.pichkhadze@gmail.com>
2023-09-22 12:42:30 +0300

    remove the min usd coin limit, and allow coins you dont have

--- app/src/main/java/com/vippygames/bianic/rebalancing/validation/BinanceRecordsValidation.java
+++ app/src/main/java/com/vippygames/bianic/rebalancing/validation/BinanceRecordsValidation.java
@@ -3,33 +3,21 @@ package com.vippygames.bianic.rebalancing.validation;
 import android.widget.Toast;
 
 import com.vippygames.bianic.MainActivity;
-import com.vippygames.bianic.consts.BinanceRecordsValidationConsts;
 import com.vippygames.bianic.db.threshold_allocation.ThresholdAllocationDb;
 import com.vippygames.bianic.db.threshold_allocation.ThresholdAllocationRecord;
 import com.vippygames.bianic.exception_handle.CriticalExceptionHandler;
 import com.vippygames.bianic.exception_handle.ExceptionHandler;
 import com.vippygames.bianic.exception_handle.exceptions.CriticalException;
 import com.vippygames.bianic.rebalancing.BinanceManager;
-import com.vippygames.bianic.rebalancing.api.coins_amount.CoinAmount;
-import com.vippygames.bianic.rebalancing.api.coins_amount.exceptions.CoinsAmountParseException;
-import com.vippygames.bianic.rebalancing.api.coins_price.CoinPrice;
-import com.vippygames.bianic.rebalancing.api.coins_price.exceptions.CoinsPriceParseException;
 import com.vippygames.bianic.rebalancing.api.common.exceptions.EmptyResponseBodyException;
 import com.vippygames.bianic.rebalancing.api.common.exceptions.FailedRequestStatusException;
 import com.vippygames.bianic.rebalancing.api.common.network_request.exceptions.NetworkRequestException;
-import com.vippygames.bianic.rebalancing.api.common.network_request.exceptions.SignatureGenerationException;
 import com.vippygames.bianic.rebalancing.api.exchange_info.ExchangeInfo;
 import com.vippygames.bianic.rebalancing.api.exchange_info.exceptions.ExchangeInfoParseException;
-import com.vippygames.bianic.rebalancing.data_format.CoinDetails;
-import com.vippygames.bianic.rebalancing.data_format.CoinsDetailsBuilder;
-import com.vippygames.bianic.rebalancing.data_format.exceptions.CoinsDetailsBuilderException;
 import com.vippygames.bianic.rebalancing.validation.exceptions.FailedValidateRecordsException;
-import com.vippygames.bianic.shared_preferences.exceptions.KeyNotFoundException;
 
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 public class BinanceRecordsValidation {
     private final MainActivity mainActivity;
@@ -40,30 +28,19 @@ public class BinanceRecordsValidation {
 
     public boolean validateRecordsBinance() {
         try {
-            BinanceManager binanceManager = new BinanceManager(mainActivity);
-
-            Map<String, ExchangeInfo> exchangeInfo = binanceManager.getExchangeInfo();
-
             ThresholdAllocationDb db = new ThresholdAllocationDb(mainActivity);
             List<ThresholdAllocationRecord> records = db.loadRecords(db.getRecords());
 
-            List<CoinAmount> coinsAmount = binanceManager.getCoinsAmount();
-            validateCoinsFoundInPortfolio(records, coinsAmount);
+            BinanceManager binanceManager = new BinanceManager(mainActivity);
+            Map<String, ExchangeInfo> exchangeInfo = binanceManager.getExchangeInfo();
 
-            List<CoinPrice> coinsPrice = binanceManager.getCoinsPrice(records);
-
-            CoinsDetailsBuilder coinsDetailsBuilder = new CoinsDetailsBuilder();
-            List<CoinDetails> coinsDetails = coinsDetailsBuilder.getCoinsDetails(records, coinsAmount, coinsPrice);
-
-            validateCoinsAboveMinUsd(coinsDetails);
+            validateCoinsInExchangeInfo(records, exchangeInfo);
 
             return true;
 
-        } catch (NetworkRequestException | FailedRequestStatusException | EmptyResponseBodyException
-                 | SignatureGenerationException | CoinsPriceParseException
-                 | CoinsAmountParseException | CoinsDetailsBuilderException
-                 | KeyNotFoundException | FailedValidateRecordsException |
-                 ExchangeInfoParseException e) {
+        } catch (NetworkRequestException | FailedRequestStatusException
+                 | EmptyResponseBodyException | FailedValidateRecordsException
+                 | ExchangeInfoParseException e) {
             ExceptionHandler exceptionHandler = new ExceptionHandler(mainActivity);
             exceptionHandler.handleException(e);
         } catch (Exception e) {
@@ -76,35 +53,14 @@ public class BinanceRecordsValidation {
         return false;
     }
 
-    private void validateCoinsFoundInPortfolio(List<ThresholdAllocationRecord> records, List<CoinAmount> coinsAmount) throws FailedValidateRecordsException {
-        Set<String> foundSymbols = new HashSet<>();
-        for (CoinAmount coinAmount : coinsAmount) {
-            foundSymbols.add(coinAmount.getSymbol());
-        }
-
+    private void validateCoinsInExchangeInfo(List<ThresholdAllocationRecord> records, Map<String, ExchangeInfo> exchangeInfo) throws FailedValidateRecordsException {
         for (ThresholdAllocationRecord record : records) {
             String coinSymbol = record.getSymbol();
-            if (!foundSymbols.contains(record.getSymbol())) {
-                String message = "You don't have the coin '" + coinSymbol + "' in your portfolio";
+            if (!exchangeInfo.containsKey(record.getSymbol())) {
+                String message = "Coin '" + coinSymbol + "' not found on Binance exchange";
                 showToast(message, Toast.LENGTH_SHORT);
                 throw new FailedValidateRecordsException(message);
             }
-        }
-    }
-
-    private void validateCoinsAboveMinUsd(List<CoinDetails> coinsDetails) throws FailedValidateRecordsException {
-        for (CoinDetails coinDetails : coinsDetails) {
-            if (coinDetails.getTotalUsdValue() < BinanceRecordsValidationConsts.MIN_COIN_VALUE_USDT) {
-                showToast("Low on '" + coinDetails.getSymbol() + "'. Min is "
-                        + BinanceRecordsValidationConsts.MIN_COIN_VALUE_USDT + "$ but you have "
-                        + coinDetails.getTotalUsdValue() + "$.", Toast.LENGTH_LONG);
-
-                throw new FailedValidateRecordsException("Value of '" + coinDetails.getSymbol()
-                        + "' in USD in your portfolio is too small - min is "
-                        + BinanceRecordsValidationConsts.MIN_COIN_VALUE_USDT + "$ but you have "
-                        + coinDetails.getTotalUsdValue() + "$.");
-            }
-
         }
     }
 
