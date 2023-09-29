bcd061e7b24b8df5f779425f06384d90b20f8c51
vipnet1 <timur.pichkhadze@gmail.com>
2023-09-29 14:49:18 +0300

    added backup option of files, excluding encrypted prefs

--- app/src/main/java/com/vippygames/bianic/consts/SharedPrefsConsts.java
+++ app/src/main/java/com/vippygames/bianic/consts/SharedPrefsConsts.java
@@ -1,8 +1,11 @@
 package com.vippygames.bianic.consts;
 
+import com.vippygames.bianic.R;
+
 public abstract class SharedPrefsConsts {
-    public static final String SHARED_PREFS_FILE = "DefaultPrefs";
-    public static final String SECRET_SHARED_PREFS_FILE = "SecretPrefs";
+    public static final String SHARED_PREFS_FILE = "regular_prefs";
+    // if change should change also in backup_rules.xml and data_extraction_rules.xml
+    public static final String SECRET_SHARED_PREFS_FILE = "encrypted_prefs";
 
     public static final String THEME_KEY = "theme";
     public static final String BINANCE_API_KEY = "binance_api_key";
